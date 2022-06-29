ac09877aeeb5f8b1f98a5e215c935c8fc609983c
Quoc Do <quocboy159@gmail.com>
2022-06-29 15:46:26 +0700

    implement replace ifelse statement to tuple list - refactor code

---
+++ ImageResizeApp/Extentions/CollectionExtentions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageResizeApp.Extentions
+{
+    public static class CollectionExtentions
+    {
+        public static IEnumerable<(T item, int index)> LoopIndex<T>(this IEnumerable<T> items)
+        {
+            return items.Select((x, i) => (x, i));
+        }
+    }
+}
--- ImageResizeApp/Program.cs
+++ ImageResizeApp/Program.cs
@@ -3,30 +3,30 @@
 using ImageResizeApp.Helper;
 using System.Drawing;
 using System.Drawing.Imaging;
+using ImageResizeApp.Extentions;
 
 Console.WriteLine("The App is running ....");
 
 var path = Environment.CurrentDirectory.Substring(0, Environment.CurrentDirectory.IndexOf("bin"));
 var fileNames = Directory.EnumerateFiles(@$"{path}input");
 
-foreach (var fileName in fileNames)
+List<(Predicate<Image> predicate, Action<Image, string> action)> imageHandlingMappers = new()
 {
-    using Image image = Image.FromFile(fileName);
-    var width = image.Width;
-    var height = image.Height;
-
     // image has width and height large than or equal 2048
-    if (width >= 2048 && height >= 2048)
+    ((Image image) => image.Width >= 2048 && image.Height >= 2048,
+    (Image image, string fileName) =>
     {
         var edge = image.Width > image.Height ? image.Height / 2 : image.Width / 2;
         var bmp = ImageHelpers.ChangeImageToSquare(image, edge);
         var newFileName = @$"{path}output\{ImageHelpers.InsertSuffixForFileName(fileName, "-thumb")}";
         bmp.Save(newFileName, ImageFormat.Jpeg);
     }
+    ),
 
     // image has width or height less than 2048
-    else if ((width < 2048 || height < 2048)
-            && (width > height ? (width / height) == 2 : (height / width) == 2))
+    ((Image image) => (image.Width < 2048 || image.Height < 2048)
+                       && (image.Width > image.Height ? (image.Width / image.Height) == 2 : (image.Height / image.Width) == 2),
+    (Image image, string fileName) =>
     {
         var edge = image.Width > image.Height ? image.Height : image.Width;
         var bmp = ImageHelpers.ChangeImageToSquare(image, edge);
@@ -35,18 +35,70 @@ foreach (var fileName in fileNames)
         bmp.Save(newFileName1, ImageFormat.Jpeg);
         bmp.Save(newFileName2, ImageFormat.Jpeg);
     }
+    ),
 
     // others cases
-    else
+    ((Image image) => true,
+    (Image image, string fileName) =>
     {
-        var bmp = ImageHelpers.ResizeImage(image, width / 2, height / 2);
+        var bmp = ImageHelpers.ResizeImage(image, image.Width / 2, image.Height / 2);
         var newFileName = @$"{path}output\{ImageHelpers.InsertSuffixForFileName(fileName, string.Empty)}";
         bmp.Save(newFileName, ImageFormat.Jpeg);
     }
+    )
+};
+
+foreach (var item in fileNames.LoopIndex())
+{
+    Console.WriteLine($"The {item.item} {item.index} is running ....");
+    var fileName = item.item;
+    using Image image = Image.FromFile(fileName);
+    if (imageHandlingMappers.Any(x => x.predicate(image)))
+    {
+        imageHandlingMappers.FirstOrDefault(x => x.predicate(image)).action(image, fileName);
+    }
 }
+
+
+//foreach (var fileName in fileNames)
+//{
+//    using Image image = Image.FromFile(fileName);
+//    var width = image.Width;
+//    var height = image.Height;
+
+//    // image has width and height large than or equal 2048
+//    if (width >= 2048 && height >= 2048)
+//    {
+//        var edge = image.Width > image.Height ? image.Height / 2 : image.Width / 2;
+//        var bmp = ImageHelpers.ChangeImageToSquare(image, edge);
+//        var newFileName = @$"{path}output\{ImageHelpers.InsertSuffixForFileName(fileName, "-thumb")}";
+//        bmp.Save(newFileName, ImageFormat.Jpeg);
+//    }
+
+//    // image has width or height less than 2048
+//    else if ((width < 2048 || height < 2048)
+//            && (width > height ? (width / height) == 2 : (height / width) == 2))
+//    {
+//        var edge = image.Width > image.Height ? image.Height : image.Width;
+//        var bmp = ImageHelpers.ChangeImageToSquare(image, edge);
+//        var newFileName1 = @$"{path}output\{ImageHelpers.InsertSuffixForFileName(fileName, "-1")}";
+//        var newFileName2 = @$"{path}output\{ImageHelpers.InsertSuffixForFileName(fileName, "-2")}";
+//        bmp.Save(newFileName1, ImageFormat.Jpeg);
+//        bmp.Save(newFileName2, ImageFormat.Jpeg);
+//    }
+
+//    // others cases
+//    else
+//    {
+//        var bmp = ImageHelpers.ResizeImage(image, width / 2, height / 2);
+//        var newFileName = @$"{path}output\{ImageHelpers.InsertSuffixForFileName(fileName, string.Empty)}";
+//        bmp.Save(newFileName, ImageFormat.Jpeg);
+//    }
+//}
 
 Console.WriteLine("Successful!!!");
 
 
 Console.ReadKey();
+
 
